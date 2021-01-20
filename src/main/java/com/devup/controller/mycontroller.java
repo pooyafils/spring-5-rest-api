76f5fe1f0f4f3ba270748ea687c40ae24f8741e0
pooyafils <pooyafils@gmail.com>
2021-01-20 10:41:11 +0330

    adding jpa

--- src/main/java/com/devup/controller/mycontroller.java
+++ src/main/java/com/devup/controller/mycontroller.java
@@ -1,18 +1,27 @@
 package com.devup.controller;
 
 import com.devup.model.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devup.repository.ProductRepository;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class mycontroller {
+    ProductRepository productRepository;
+
+    public mycontroller(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @GetMapping
     public Product seemyname(){
         Product product=new Product();
         product.setId(1);
         product.setName("d");
         return product;
+    }
+    @PostMapping
+    public Product postproduct(@RequestBody Product product){
+     return    productRepository.save(product);
     }
 }
