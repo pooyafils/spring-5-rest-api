fb8f6af28526fad342722d833a8071d8a6b378ca
pooyafils <pooyafils@gmail.com>
2021-01-24 18:36:44 +0330

    adding swagger to the api

--- src/main/java/com/devup/config/MyAppConfig.java
+++ src/main/java/com/devup/config/MyAppConfig.java
@@ -15,11 +15,20 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.Properties;
-
+@EnableSwagger2
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.devup")
@@ -72,7 +81,34 @@ public class MyAppConfig implements WebMvcConfigurer {
                 localContainerEntityManagerFactoryBean().getObject());
         return jpaTransactionManager;
     }
+@Bean
+    public Docket api(){
+    return new Docket(DocumentationType.SWAGGER_2)
+            .select().apis(RequestHandlerSelectors.any())
+            .build().pathMapping("/")
+            .apiInfo(metaData());
 
+}
+    private ApiInfo metaData(){
+
+        Contact contact = new Contact("pooya-fils", "https://github.com/pooyafils","" +
+                "mygithub");
+
+        return new ApiInfo(
+                "spring mvc rest ",
+                " product api",
+                "1.0",
+                "Terms of Service",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 
 
 }
